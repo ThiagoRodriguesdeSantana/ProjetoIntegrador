53ff6ff4ccd413238c9c20bb00eb9cbaf8b4849b
Thiago Santana <thiagorodriguescamara@gmail.com>
2016-06-09 23:27:38 -0300

    criacao_da_classe_student

--- Sigma/src/controller/Student.java
+++ Sigma/src/controller/Student.java
@@ -9,74 +9,19 @@ public class Student extends People{
     private long senha;
     private String situacao;
 
-    /**
-     * @return the registrartion
-     */
-    public long getRegistrartion() {
-        return registrartion;
-    }
-
-    /**
-     * @param registrartion the registrartion to set
-     */
-    public void setRegistrartion(long registrartion) {
-        this.registrartion = registrartion;
-    }
-
-    /**
-     * @return the course
-     */
-    public String getCourse() {
-        return course;
-    }
-
-    /**
-     * @param course the course to set
-     */
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    /**
-     * @return the login
-     */
-    public String getLogin() {
-        return login;
-    }
-
-    /**
-     * @param login the login to set
-     */
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    /**
-     * @return the senha
-     */
-    public long getSenha() {
-        return senha;
-    }
-
-    /**
-     * @param senha the senha to set
-     */
-    public void setSenha(long senha) {
-        this.senha = senha;
-    }
-
-    /**
-     * @return the situacao
-     */
-    public String getSituacao() {
-        return situacao;
-    }
-
-    /**
-     * @param situacao the situacao to set
-     */
-    public void setSituacao(String situacao) {
-        this.situacao = situacao;
+    public long getRegistrartion() {return registrartion;}
+    public void setRegistrartion(long registrartion) { this.registrartion = registrartion;}
+    public String getCourse() { return course;}
+    public void setCourse(String course) { this.course = course;}
+    public String getLogin() { return login; }
+    public void setLogin(String login) {this.login = login;}
+    public long getSenha() { return senha; }
+    public void setSenha(long senha) {this.senha = senha; }
+    public String getSituacao() {return situacao;}
+    public void setSituacao(String situacao) { this.situacao = situacao; }
+    
+    public void CourseRegister(){
+        
     }
     
     
