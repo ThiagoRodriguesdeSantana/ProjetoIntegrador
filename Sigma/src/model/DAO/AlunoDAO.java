cd23269316f85cb68f387ea22fb18ffab252031e
Brunoroh <bruno_hat@live.com>
2016-06-12 19:07:36 -0300

    Dao aluno

--- Sigma/src/model/DAO/AlunoDAO.java
+++ Sigma/src/model/DAO/AlunoDAO.java
@@ -1,6 +1,9 @@
 package model.DAO;
 
+import controller.Aluno;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import model.Conexao;
 
 public class AlunoDAO {
@@ -8,5 +11,16 @@ public class AlunoDAO {
     
     public AlunoDAO(){
         conectar = Conexao.getConexao();
+    }
+    
+    public void incluir(Aluno aluno)throws SQLException{
+        String comando = "insert into Contatos(nome,telefone,email,endereco,cpf)values(?,?,?,?,?)";
+        PreparedStatement ps = conectar.prepareStatement(comando);
+        ps.setString(1,aluno.getNome());
+        ps.setLong(2, aluno.getTelefone());
+        ps.setString(3,aluno.getEmail());
+        ps.setString(4,aluno.getEndereco());
+        ps.setLong(5, aluno.getCPF());
+        ps.executeUpdate();
     }
 }
