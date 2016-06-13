0797bfefae0c891d4cf0ddea4a28944cbb0147bd
Thiago Santana <thiagorodriguescamara@gmail.com>
2016-06-12 21:28:58 -0300

    Finalizacao da classe DAO Professor

--- Sigma/src/controller/Professor.java
+++ Sigma/src/controller/Professor.java
@@ -3,20 +3,21 @@ package controller;
 
 public class Professor extends Pessoa{
     
-    private String titulacao;
+    private int titulacao;
     
 
-    public Professor(int id, String nome, String cpf, String endereco,String telefone, String senha, int tipo,String titulacao) {
+    public Professor(int id, String nome, String cpf, String endereco,String telefone, String senha, int tipo,int titulacao) {
         super(id, nome, cpf, endereco,telefone, senha, tipo);
         this.titulacao = titulacao;
     }
-
-    public Professor(int aInt, String string, String string0, String string1, String string2, int aInt0, String string3) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Professor(){
+        
     }
 
-    public String getTitulacao() { return titulacao;}
-    public void setTitulacao(String titulacao) { this.titulacao = titulacao;}
+   
+
+    public int getTitulacao() { return titulacao;}
+    public void setTitulacao(int titulacao) { this.titulacao = titulacao;}
   
     
 }
