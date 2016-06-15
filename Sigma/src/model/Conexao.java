470d1af4a7e583a212802bfca1ceb06e5ee4be42
Brunoroh <bruno_hat@live.com>
2016-06-15 00:05:35 -0300

    correcao 2

--- Sigma/src/model/Conexao.java
+++ Sigma/src/model/Conexao.java
@@ -16,30 +16,11 @@ public class Conexao {
     
         else{
             try{
-//                String driver = "com.mysql.jdbc.Driver";
-//                String url = "jdbc:mysql://localhost/sigma";
-//<<<<<<< HEAD
-//                String user = "sigma";
-//                String password = "bancodedados";
-//=======
-//                String user = "root";
-//                String password = "root12";
-//>>>>>>> c5f99e1303a3ffbac1bd86bd4bd7b04beb6218a0
                 String driver = "com.mysql.jdbc.Driver";
-<<<<<<< HEAD
                 String url = "jdbc:mysql://127.0.0.1:3306/sigma";
                 String user = "root";  
                 String password = "bancodedados";
-               
-=======
-                String url = "jdbc:mysql://localhost/sigma";
-
-
-                String user = "root";
-                String password = "root12";
-
             
->>>>>>> 3a5a619eab6e9828cdd2bd08586883098cf9b526
                 Class.forName(driver);
                 conexao = DriverManager.getConnection(url,user,password);
 
